dcf424af7d82f7c1776e444334b6dbacff436945
watanavex <susan.se3p@gmail.com>
2020-03-30 20:12:46 +0900

    Exampleプロジェクトを作成

---
+++ Example/Example/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by 渡部 陽太 on 2020/03/30.
+//  Copyright © 2020 YUMEMI Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
