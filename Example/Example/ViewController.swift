b65a3671a77c78685ddfdc3a99c7d108fd1c889e
watanavex <susan.se3p@gmail.com>
2020-03-31 15:35:04 +0900

    IBOutlet, IBAction接続

--- Example/Example/ViewController.swift
+++ Example/Example/ViewController.swift
@@ -10,11 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var weatherImageView: UIImageView!
+    @IBOutlet weak var minTempLabel: UILabel!
+    @IBOutlet weak var maxTempLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func onTouchReload(_ sender: Any) {
+    }
+    
 }
 
