fa15b4720fd8bc72322c1f66206523603afd3653
watanavex <susan.se3p@gmail.com>
2020-04-01 09:34:57 +0900

    天気予報取得処理をModelクラスに切り出し

---
+++ Example/Example/Data/Response.swift
@@ -0,0 +1,16 @@
+//
+//  Response.swift
+//  Example
+//
+//  Created by 渡部 陽太 on 2020/03/31.
+//  Copyright © 2020 YUMEMI Inc. All rights reserved.
+//
+
+import Foundation
+
+struct Response: Decodable {
+    let weather: String
+    let maxTemp: Int
+    let minTemp: Int
+    let date: Date
+}
