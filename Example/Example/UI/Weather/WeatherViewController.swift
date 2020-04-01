d2e740c1d62f8c80a7cccf3b55826e37c18ad2e9
watanavex <susan.se3p@gmail.com>
2020-04-01 10:00:25 +0900

    Modelクラスを利用した実装にリファクタリング

--- Example/Example/UI/Weather/WeatherViewController.swift
+++ Example/Example/UI/Weather/WeatherViewController.swift
@@ -11,80 +11,34 @@ import YumemiWeather
 
 class WeatherViewController: UIViewController {
     
-    struct JsonDecodeError: Error {
-    }
-
+    var weatherModel: WeatherModel!
     @IBOutlet weak var weatherImageView: UIImageView!
     @IBOutlet weak var minTempLabel: UILabel!
     @IBOutlet weak var maxTempLabel: UILabel!
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
     
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
+    @IBAction func onTapReload(_ sender: Any) {
         loadWeather()
     }
     
-    @IBAction func unwind(segue: UIStoryboardSegue) {
-        
-    }
-
     func loadWeather() {
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZZZZZ"
-        
-        let requestJson = """
-        {
-            "area": "tokyo",
-            "date": "\(dateFormatter.string(from: Date()))"
-        }
-        """
-        
+        let request = Request(area: "tokyo", date: Date())
         do {
-            let responseJson = try YumemiWeather.fetchWeather(requestJson)
-            guard let responseData = responseJson.data(using: .utf8) else {
-                throw JsonDecodeError()
-            }
-            
-            let responseJsonObject = try JSONSerialization.jsonObject(with: responseData, options: [])
-            guard let responseDictionary = responseJsonObject as? [String: Any] else {
-                throw JsonDecodeError()
-            }
-            
-            guard let weatherString = responseDictionary["weather"] as? String,
-                let weather = Weather(rawValue: weatherString) else {
-                throw JsonDecodeError()
-            }
-            guard let minTemp = responseDictionary["min_temp"] as? Int else {
-                throw JsonDecodeError()
-            }
-            guard let maxTemp = responseDictionary["max_temp"] as? Int else {
-                throw JsonDecodeError()
-            }
-            
-            switch weather {
-            case .sunny:
-                self.weatherImageView.image = R.image.sunny()
-                self.weatherImageView.tintColor = R.color.red()
-            case .cloudy:
-                self.weatherImageView.image = R.image.cloudy()
-                self.weatherImageView.tintColor = R.color.gray()
-            case .rainy:
-                self.weatherImageView.image = R.image.rainy()
-                self.weatherImageView.tintColor = R.color.blue()
-            }
-            self.minTempLabel.text = String(minTemp)
-            self.maxTempLabel.text = String(maxTemp)
+            let response = try self.weatherModel.fetchWeather(request)
+            self.weatherImageView.set(weather: response.weather)
+            self.minTempLabel.text = String(response.minTemp)
+            self.maxTempLabel.text = String(response.maxTemp)
         }
         catch {
             let message: String
             switch error {
-            case is JsonDecodeError:
-                message = "天気予報が読み取れませんでした。"
+            case let error as WeatherModelError where error ~= WeatherModelError.jsonEncodeError:
+                message = "Jsonエンコードに失敗しました。"
+            case let error as WeatherModelError where error ~= WeatherModelError.jsonDecodeError:
+                message = "Jsonデコードに失敗しました。"
             case let error as YumemiWeatherError where error ~= YumemiWeatherError.unknownError:
                 message = "エラーが発生しました。"
             case let error as YumemiWeatherError where error ~= YumemiWeatherError.invalidParameterError:
@@ -100,3 +54,18 @@ class WeatherViewController: UIViewController {
     
 }
 
+private extension UIImageView {
+    func set(weather: Weather) {
+        switch weather {
+        case .sunny:
+            self.image = R.image.sunny()
+            self.tintColor = R.color.red()
+        case .cloudy:
+            self.image = R.image.cloudy()
+            self.tintColor = R.color.gray()
+        case .rainy:
+            self.image = R.image.rainy()
+            self.tintColor = R.color.blue()
+        }
+    }
+}
