066f92650d6b68f1179a26e41ccb7cc219d14706
watanavex <susan.se3p@gmail.com>
2020-04-02 10:42:33 +0900

    watanave initial commit

---
+++ Watanavex/Watanavex/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Watanavex
+//
+//  Created by 渡部 陽太 on 2020/04/02.
+//  Copyright © 2020 YUMEMI Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
