13256923ad5aeab7b9ab51e3f1cf280d6a5cbced
Daichi Hayashi <daichikuwa0618@gmail.com>
2020-04-02 10:48:41 +0900

    make project, add package

---
+++ d_hayashi/weatherApp/weatherApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  weatherApp
+//
+//  Created by 林 大地 on 2020/04/02.
+//  Copyright © 2020 Daichi Hayashi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ d_hayashi/weatherApp/weatherAppTests/weatherAppTests.swift
@@ -0,0 +1,34 @@
+//
+//  weatherAppTests.swift
+//  weatherAppTests
+//
+//  Created by 林 大地 on 2020/04/02.
+//  Copyright © 2020 Daichi Hayashi. All rights reserved.
+//
+
+import XCTest
+@testable import weatherApp
+
+class weatherAppTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
