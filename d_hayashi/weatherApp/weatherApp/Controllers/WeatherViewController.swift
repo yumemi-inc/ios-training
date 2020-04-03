a15f91e58aed427bbec099b9ca279a9a04e0482e
Daichi Hayashi <daichikuwa0618@gmail.com>
2020-04-03 09:38:56 +0900

    YumemiWeather ライブラリのインポート および 適当な入力を渡して標準出力する処理を追加

--- d_hayashi/weatherApp/weatherApp/Controllers/WeatherViewController.swift
+++ d_hayashi/weatherApp/weatherApp/Controllers/WeatherViewController.swift
@@ -7,12 +7,21 @@
 //
 
 import UIKit
+import YumemiWeather
 
 class WeatherViewController: UIViewController {
+    
+    private let inputString = "{ \"area\": \"tokyo\", \"date\": \"2020-04-01T12:00:00+09:00\" }"
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        do {
+            try print(YumemiWeather.fetchWeather(inputString))
+        } catch {
+            print("error")
+        }
     }
 
 
