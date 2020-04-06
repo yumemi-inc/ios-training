907eac44eed0943be0fdbde8d484927a7a1e3ce5
Daichi Hayashi <daichikuwa0618@gmail.com>
2020-04-06 14:25:37 +0900

    FirstView が表示された後に、WeatherView を表示するように変更

--- d_hayashi/weatherApp/weatherApp/Controllers/FirstViewController.swift
+++ d_hayashi/weatherApp/weatherApp/Controllers/FirstViewController.swift
@@ -10,4 +10,18 @@ import UIKit
 
 class FirstViewController: UIViewController {
 
+    // View の構築
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // View 構築後の処理
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+
+        let weatherViewStoryboard: UIStoryboard = UIStoryboard(name: "WeatherView", bundle: nil)
+        let weatherViewController: UIViewController = weatherViewStoryboard.instantiateViewController(withIdentifier: "WeatherViewController")
+        self.present(weatherViewController, animated: true, completion: nil)
+    }
+
 }
