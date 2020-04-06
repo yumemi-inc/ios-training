808a910c65229f94d0021fd0b75ac6052bdd0036
Daichi Hayashi <daichikuwa0618@gmail.com>
2020-04-06 13:24:04 +0900

    新規 VC 及び Storyboard を追加

---
+++ d_hayashi/weatherApp/weatherApp/Controllers/FirstViewController.swift
@@ -0,0 +1,18 @@
+//
+//  FirstViewController.swift
+//  weatherApp
+//
+//  Created by 林 大地 on 2020/04/06.
+//  Copyright © 2020 Daichi Hayashi. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.view.backgroundColor = .yellow
+    }
+}
