0495230cc64669f6d82ccc494c6d48732f2a0407
Daichi Hayashi <daichikuwa0618@gmail.com>
2020-04-07 20:19:01 +0900

    GetWeatherResource に enum を追加

--- d_hayashi/weatherApp/weatherApp/Models/GetWeatherResourceName.swift
+++ d_hayashi/weatherApp/weatherApp/Models/GetWeatherResourceName.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import UIKit
 
 final class GetWeatherResourceName {
 
@@ -43,6 +44,28 @@ final class GetWeatherResourceName {
 
         default:
             throw WeatherAppError.unknownAppError
+        }
+    }
+}
+
+enum GetWeatherResourceEnum: String {
+    case sunny
+    case cloudy
+    case rainy
+
+    var color: UIColor {
+        switch self {
+        case .sunny: return UIColor(named: "sunny") ?? .red
+        case .cloudy: return UIColor(named: "cloudy") ?? .gray
+        case .rainy: return UIColor(named: "rainy") ?? .blue
+        }
+    }
+
+    var image: UIImage {
+        switch self {
+        case .sunny: return #imageLiteral(resourceName: "sunny")
+        case .cloudy: return #imageLiteral(resourceName: <#T##String#>)
+        case .rainy: return #imageLiteral(resourceName: "rainy")
         }
     }
 }
