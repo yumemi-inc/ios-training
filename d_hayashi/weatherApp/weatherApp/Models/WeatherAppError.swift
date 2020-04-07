985aac7cfae6a8afeb4638ebcc3a5f216fde21c4
Daichi Hayashi <daichikuwa0618@gmail.com>
2020-04-07 17:04:59 +0900

    WeatherAppError を Swift.Error 型とし、LocalizedError として extension  するように修正

--- d_hayashi/weatherApp/weatherApp/Models/WeatherAppError.swift
+++ d_hayashi/weatherApp/weatherApp/Models/WeatherAppError.swift
@@ -8,10 +8,28 @@
 
 import Foundation
 
-enum WeatherAppError: LocalizedError {
+enum WeatherAppError: Error {
 
     case unknownAppError
     case jsonEncodeAppError
     case jsonDecodeAppError
     case invalidParameterAppError
+}
+
+extension WeatherAppError: LocalizedError {
+
+    var errorDescription: String? {
+
+        switch self {
+
+        case .unknownAppError:
+            return "unknown error happend"
+        case .jsonEncodeAppError:
+            return "encoding input parameter to json error"
+        case .jsonDecodeAppError:
+            return "decoding json responce to string error"
+        case .invalidParameterAppError:
+            return "input parameter is wrong"
+        }
+    }
 }
