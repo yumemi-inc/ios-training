4d903929843cb0c676772cd9bc4ca4c61683d298
Daichi Hayashi <daichikuwa0618@gmail.com>
2020-04-08 19:44:46 +0900

    WeatherResponce から UIKit import を削除。color および image の責務を切り分け、View/WeatherIconResource に extension として移動

--- d_hayashi/weatherApp/weatherApp/Models/WeatherResponse.swift
+++ d_hayashi/weatherApp/weatherApp/Models/WeatherResponse.swift
@@ -7,7 +7,6 @@
 //
 
 import Foundation
-import UIKit
 
 struct WeatherResponse: Decodable {
 
@@ -15,31 +14,4 @@ struct WeatherResponse: Decodable {
     let minTemp: Int
     let date: Date
     let weather: Weather
-
-    enum Weather: String, Decodable {
-
-        case sunny
-        case cloudy
-        case rainy
-
-        var color: UIColor {
-
-            switch self {
-
-            case .sunny: return UIColor(named: "sunny") ?? .red
-            case .cloudy: return UIColor(named: "cloudy") ?? .gray
-            case .rainy: return UIColor(named: "rainy") ?? .blue
-            }
-        }
-
-        var image: UIImage {
-
-            switch self {
-
-            case .sunny: return #imageLiteral(resourceName: "sunny")
-            case .cloudy: return #imageLiteral(resourceName: "cloudy")
-            case .rainy: return #imageLiteral(resourceName: "rainy")
-            }
-        }
-    }
 }
---
+++ d_hayashi/weatherApp/weatherApp/Views/WeatherIconResource.swift
@@ -0,0 +1,40 @@
+//
+//  WeatherIconResource.swift
+//  weatherApp
+//
+//  Created by 林 大地 on 2020/04/08.
+//  Copyright © 2020 Daichi Hayashi. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension WeatherResponse {
+
+    enum Weather: String, Decodable {
+
+        case sunny
+        case cloudy
+        case rainy
+
+        var color: UIColor {
+
+            switch self {
+
+            case .sunny: return UIColor(named: "sunny") ?? .red
+            case .cloudy: return UIColor(named: "cloudy") ?? .gray
+            case .rainy: return UIColor(named: "rainy") ?? .blue
+            }
+        }
+
+        var image: UIImage {
+
+            switch self {
+
+            case .sunny: return #imageLiteral(resourceName: "sunny")
+            case .cloudy: return #imageLiteral(resourceName: "cloudy")
+            case .rainy: return #imageLiteral(resourceName: "rainy")
+            }
+        }
+    }
+}
