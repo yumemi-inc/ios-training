124f87588ea83cb4a69c668b00d89fa8d2613e6c
watanavex <y_watanabe@yumemi.co.jp>
2020-04-08 14:42:18 +0900

    起動時のViewControllerを変更

--- Example/Example/SceneDelegate.swift
+++ Example/Example/SceneDelegate.swift
@@ -20,9 +20,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: windowScene)
         self.window = window
-        let viewController = R.storyboard.weather.instantiateInitialViewController()!
-        viewController.weatherModel = WeatherModelImpl()
-        window.rootViewController = viewController
+        window.rootViewController = RootViewController()
         window.makeKeyAndVisible()
     }
 
---
+++ Example/Example/UI/Root/RootViewController.swift
@@ -0,0 +1,35 @@
+//
+//  RootViewController.swift
+//  Example
+//
+//  Created by 渡部 陽太 on 2020/04/01.
+//  Copyright © 2020 YUMEMI Inc. All rights reserved.
+//
+
+import UIKit
+
+class RootViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        let viewController = R.storyboard.weather.instantiateInitialViewController()!
+        viewController.weatherModel = WeatherModelImpl()
+        viewController.modalPresentationStyle = .fullScreen
+        present(viewController, animated: true, completion: nil)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
