23e27d8c5e12e7af9a54a4cdc4b90cfc7a302519
Daichi Hayashi <daichikuwa0618@gmail.com>
2020-04-10 10:57:59 +0900

    天気予報を取得する getWeather が定義された protocol: WeatherModel を作成

---
+++ d_hayashi/weatherApp/weatherApp/Models/WeatherModel.swift
@@ -0,0 +1,14 @@
+//
+//  WeatherModel.swift
+//  weatherApp
+//
+//  Created by 林 大地 on 2020/04/10.
+//  Copyright © 2020 Daichi Hayashi. All rights reserved.
+//
+
+import Foundation
+
+protocol WeatherModel {
+
+    func getWeather(_ area: String)
+}
