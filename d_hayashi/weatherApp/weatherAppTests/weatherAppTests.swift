f563fa308ad6c7c2c5f511838a18c580be6ac928
Daichi Hayashi <daichikuwa0618@gmail.com>
2020-04-13 11:19:39 +0900

    UnitTest で WeatherViewController をインスタンス化

--- d_hayashi/weatherApp/weatherAppTests/weatherAppTests.swift
+++ d_hayashi/weatherApp/weatherAppTests/weatherAppTests.swift
@@ -11,8 +11,13 @@ import XCTest
 
 class weatherAppTests: XCTestCase {
 
+    var weatherViewController = WeatherViewController()
+
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+
+        let weatherStoryboard = UIStoryboard(name: "WeatherViewController", bundle: nil)
+        weatherViewController = weatherStoryboard.instantiateViewController(identifier: "WeatherViewController") as! WeatherViewController
+        weatherViewController.loadViewIfNeeded()
     }
 
     override func tearDownWithError() throws {
