144f312b5ee179295c41db84ed0dba22a3a64ebe
tokizuoh <16jk112@ms.dendai.ac.jp>
2020-04-14 15:08:26 +0900

    feat: ViewControllerFirstを追加。既存のVCをViewControllerSecondにrename。

---
+++ Training/iOS-Training/iOS-Training/ViewControllerFirst.swift
@@ -0,0 +1,30 @@
+//
+//  ViewControllerFirst.swift
+//  iOS-Training
+//
+//  Created by 酒井 桂哉 on 2020/04/14.
+//  Copyright © 2020 tokizuoh. All rights reserved.
+//
+
+import UIKit
+
+class ViewControllerFirst: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
