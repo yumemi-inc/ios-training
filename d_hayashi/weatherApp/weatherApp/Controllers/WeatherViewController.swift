f3a7139d663d27ff549fc4e708a4c316e703c7b3
d_hayashi <daichikuwa0618@gmail.com>
2020-04-15 21:52:55 +0900

    setActivityIndicatorProperty を VC 配下に直接配置

--- d_hayashi/weatherApp/weatherApp/Controllers/WeatherViewController.swift
+++ d_hayashi/weatherApp/weatherApp/Controllers/WeatherViewController.swift
@@ -108,3 +108,23 @@ extension WeatherViewController: WeatheModelDelegate {
         }
     }
 }
+
+// MARK: ActivityIndicator
+extension WeatherViewController {
+
+    func setActivityIndicatorProperty() {
+
+        activityIndicator.color = .gray
+        activityIndicator.style = .large
+        activityIndicator.hidesWhenStopped = true
+
+        activityIndicator.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(activityIndicator)
+
+        // MARK: - AutoLayout Constraints
+        activityIndicator.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+        activityIndicator.centerYAnchor.constraint(equalTo: self.view.centerYAnchor).isActive = true
+        activityIndicator.widthAnchor.constraint(equalTo: self.view.widthAnchor).isActive = true
+        activityIndicator.heightAnchor.constraint(equalTo: self.view.heightAnchor).isActive = true
+    }
+}
