5acf9e62517d0c55510b047fc6b558838008be77
watanavex <y_watanabe@yumemi.co.jp>
2020-04-19 10:50:19 +0900

    非同期通信中にActivityIndicatorを表示

--- Example/Example/UI/Weather/WeatherViewController.swift
+++ Example/Example/UI/Weather/WeatherViewController.swift
@@ -14,6 +14,7 @@ class WeatherViewController: UIViewController {
     @IBOutlet weak var weatherImageView: UIImageView!
     @IBOutlet weak var minTempLabel: UILabel!
     @IBOutlet weak var maxTempLabel: UILabel!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
