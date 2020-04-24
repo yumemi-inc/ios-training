b61f698a8f84e406ccf1b58649e291d527aeb37e
tokizuoh <16jk112@ms.dendai.ac.jp>
2020-04-24 16:06:18 +0900

    fix: 削除した既存メソッドの再利用

--- Training/iOS-Training/iOS-Training/WeatherModelImpl.swift
+++ Training/iOS-Training/iOS-Training/WeatherModelImpl.swift
@@ -36,32 +36,35 @@ class WeatherModelImpl: WeatherModel {
     
     func getWeather(completion: @escaping(Result<WeatherResponse, WeatherError>) -> ()) {
         DispatchQueue.global().async {
-            let parameter = WeatherParameter(area: "tokyo", date: Date())
-            do {
-                let requestData = try WeatherModelImpl.encoder.encode(parameter)
-                guard let requestJson = String(data: requestData, encoding: .utf8) else {
-                    completion(.failure(.invalidParameterError))
-                    return
-                }
-                let responseJson = try YumemiWeather.syncFetchWeather(requestJson)
-                let response = try WeatherModelImpl.decoder.decode(WeatherResponse.self, from: Data(responseJson.utf8))
-                completion(.success(response))
-            } catch is EncodingError {
-                completion(.failure(.jsonEncodeError))
-            } catch is DecodingError {
-                completion(.failure(.jsonDecodeError))
-            } catch let error as YumemiWeatherError {
-                switch error {
-                case .invalidParameterError:
-                    completion(.failure(.invalidParameterError))
-                case .jsonDecodeError:
-                    completion(.failure(.jsonDecodeError))
-                case .unknownError:
-                    completion(.failure(.unknownError))
-                }
-            } catch {
-                completion(.failure(.unknownError))
+            completion(self.getWeather())
+        }
+    }
+    
+    func getWeather() -> Result<WeatherResponse, WeatherError>{
+        let parameter = WeatherParameter(area: "tokyo", date: Date())
+        do {
+            let requestData = try WeatherModelImpl.encoder.encode(parameter)
+            guard let requestJson = String(data: requestData, encoding: .utf8) else {
+                return .failure(.invalidParameterError)
             }
+            let responseJson = try YumemiWeather.syncFetchWeather(requestJson)
+            let response = try WeatherModelImpl.decoder.decode(WeatherResponse.self, from: Data(responseJson.utf8))
+            return .success(response)
+        } catch is EncodingError {
+            return .failure(.jsonEncodeError)
+        } catch is DecodingError {
+            return .failure(.jsonDecodeError)
+        } catch let error as YumemiWeatherError {
+            switch error {
+            case .invalidParameterError:
+                return .failure(.invalidParameterError)
+            case .jsonDecodeError:
+                return .failure(.jsonDecodeError)
+            case .unknownError:
+                return .failure(.unknownError)
+            }
+        } catch {
+            return .failure(.unknownError)
         }
     }
 }
