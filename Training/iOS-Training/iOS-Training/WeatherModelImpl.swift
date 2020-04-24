02e5ff538b1948221599de407f9dc84fc9cbe95c
tokizuoh <16jk112@ms.dendai.ac.jp>
2020-04-24 12:20:13 +0900

    fix: 未使用のメソッドの削除

--- Training/iOS-Training/iOS-Training/WeatherModelImpl.swift
+++ Training/iOS-Training/iOS-Training/WeatherModelImpl.swift
@@ -30,34 +30,6 @@ class WeatherModelImpl: WeatherModel {
         return encoder
     }()
     
-    func getWeather() -> Result<WeatherResponse, WeatherError>{
-        let parameter = WeatherParameter(area: "tokyo", date: Date())
-        do {
-            let requestData = try WeatherModelImpl.encoder.encode(parameter)
-            guard let requestJson = String(data: requestData, encoding: .utf8) else {
-                return .failure(.invalidParameterError)
-            }
-            let responseJson = try YumemiWeather.syncFetchWeather(requestJson)
-            let response = try WeatherModelImpl.decoder.decode(WeatherResponse.self, from: Data(responseJson.utf8))
-            return .success(response)
-        } catch is EncodingError {
-            return .failure(.jsonEncodeError)
-        } catch is DecodingError {
-            return .failure(.jsonDecodeError)
-        } catch let error as YumemiWeatherError {
-            switch error {
-            case .invalidParameterError:
-                return .failure(.invalidParameterError)
-            case .jsonDecodeError:
-                return .failure(.jsonDecodeError)
-            case .unknownError:
-                return .failure(.unknownError)
-            }
-        } catch {
-            return .failure(.unknownError)
-        }
-    }
-    
     func generateAPIErrorMessage (error: WeatherError) -> String {
         return error.toString
     }
