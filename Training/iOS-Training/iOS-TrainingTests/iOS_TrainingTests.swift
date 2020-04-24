ee4c7b0f24dbf7be242497ac4a8c5539cf36916b
tokizuoh <16jk112@ms.dendai.ac.jp>
2020-04-24 11:39:15 +0900

    test: WeatherModelStub()をmodelに代入する処理を共通化

--- Training/iOS-Training/iOS-TrainingTests/iOS_TrainingTests.swift
+++ Training/iOS-Training/iOS-TrainingTests/iOS_TrainingTests.swift
@@ -17,6 +17,7 @@ class iOS_TrainingTests: XCTestCase {
     
     override func setUpWithError() throws {
         storyboard = UIStoryboard(name: "Main", bundle: nil)
+        weatherModel = WeatherModelStub()
     }
     
     override func tearDownWithError() throws {
@@ -25,7 +26,6 @@ class iOS_TrainingTests: XCTestCase {
     
     func test_天気がsunnyだったら_画面に晴れ画像が表示される() throws {
         let response = WeatherResponse(weather: .sunny, maxTemp: 20, minTemp: -20, date: Date())
-        weatherModel = WeatherModelStub()
         weatherModel.response = response
         
         weatherViewController = storyboard.instantiateViewController(identifier: "WeatherViewController", creator: { (coder) -> WeatherViewController? in
@@ -44,7 +44,6 @@ class iOS_TrainingTests: XCTestCase {
     
     func test_天気予報がcloudyだったら_画面に曇り画像が表示される() throws {
         let response = WeatherResponse(weather: .cloudy, maxTemp: 20, minTemp: -20, date: Date())
-        weatherModel = WeatherModelStub()
         weatherModel.response = response
         
         weatherViewController = storyboard.instantiateViewController(identifier: "WeatherViewController", creator: { (coder) -> WeatherViewController? in
@@ -63,7 +62,6 @@ class iOS_TrainingTests: XCTestCase {
     
     func test_天気予報がrainyだったら_画面に雨画像が表示される() throws {
         let response = WeatherResponse(weather: .rainy, maxTemp: 20, minTemp: -20, date: Date())
-        weatherModel = WeatherModelStub()
         weatherModel.response = response
         
         weatherViewController = storyboard.instantiateViewController(identifier: "WeatherViewController", creator: { (coder) -> WeatherViewController? in
@@ -82,7 +80,6 @@ class iOS_TrainingTests: XCTestCase {
     
     func test_天気予報の最高気温がUILabelに反映される() throws {
         let response = WeatherResponse(weather: .rainy, maxTemp: 20, minTemp: -20, date: Date())
-        weatherModel = WeatherModelStub()
         weatherModel.response = response
         
         weatherViewController = storyboard.instantiateViewController(identifier: "WeatherViewController", creator: { (coder) -> WeatherViewController? in
@@ -101,7 +98,6 @@ class iOS_TrainingTests: XCTestCase {
     
     func test_天気予報の最低い気温がUILabelに反映される() throws {
         let response = WeatherResponse(weather: .rainy, maxTemp: 20, minTemp: -20, date: Date())
-        weatherModel = WeatherModelStub()
         weatherModel.response = response
         
         weatherViewController = storyboard.instantiateViewController(identifier: "WeatherViewController", creator: { (coder) -> WeatherViewController? in
