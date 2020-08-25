a9abb6974c828ccede09153abaa89c1699bb4905
TappediOS <eu.bkw7jdcpa2r@gmail.com>
2020-08-25 13:41:02 +0900

    Storyboardで定義したViewをVCにOutlet接続した

--- j_izutsu/j_izutsu/Views/Weather/WeatherViewController.swift
+++ j_izutsu/j_izutsu/Views/Weather/WeatherViewController.swift
@@ -11,8 +11,22 @@ import UIKit
 final class WeatherViewController: UIViewController {
     private var presenter: WeatherViewPresenterProtocol!
     
+    @IBOutlet weak var weatherImageView: UIImageView!
+    @IBOutlet weak var minTemperatureLabel: UILabel!
+    @IBOutlet weak var maxTemperatureLabel: UILabel!
+    @IBOutlet weak var closeButton: UIButton!
+    @IBOutlet weak var reloadButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+    }
+    
+    @IBAction func tapReloadButton(_ sender: Any) {
+        
+    }
+    
+    @IBAction func tapCloseButton(_ sender: Any) {
         
     }
     
