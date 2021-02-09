3f0e811394ba4127230cf2efdf7b2a8aad35cefc
oonishireon <oonishireon110@gmail.com>
2021-02-09 16:23:18 +0900

    サブモジュールを消去

---
+++ oonishireon/oonishireon/oonishireon/ViewController.swift
@@ -0,0 +1,20 @@
+
+
+//
+//  ViewController.swift
+//  oonishireon
+//
+//  Created by 大西 玲音 on 2021/02/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
