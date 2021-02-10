91f73a11e4dc3be8d7ffde1048bf2a152b7d91af
oonishireon <oonishireon110@gmail.com>
2021-02-10 15:46:06 +0900

    ReloadButtonを押した時にUIImageViewにsun, cloud, umbrella画像が表示されるように変更

--- oonishireon/oonishireon/oonishireon/ViewController.swift
+++ oonishireon/oonishireon/oonishireon/ViewController.swift
@@ -8,13 +8,49 @@
 //
 
 import UIKit
+import YumemiWeather
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak private var weatherImageView: UIImageView!
+    private enum Weather: String {
+        case sunny
+        case rainy
+        case cloudy
+        enum Image: String {
+            case sun
+            case umbrella
+            case cloud
+        }
+    }
+    
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+
+    @IBAction func tappedReloadButton(_ sender: Any) {
+        let fetchedWeather = YumemiWeather.fetchWeather()
+        guard let weather = Weather(rawValue: fetchedWeather) else { return }
+        switch weather {
+        case .sunny:
+            guard let image = UIImage(named: Weather.Image.sun.rawValue) else { return }
+            weatherImageView.image = image.withTintColor(.red)
+        case .rainy:
+            guard let image = UIImage(named: Weather.Image.umbrella.rawValue) else { return }
+            weatherImageView.image = image.withTintColor(.blue)
+        case .cloudy:
+            guard let image = UIImage(named: Weather.Image.cloud.rawValue) else { return }
+            weatherImageView.image = image.withTintColor(.gray)
+        }
+    }
+    
+    
 
 
 }
+
+
 
