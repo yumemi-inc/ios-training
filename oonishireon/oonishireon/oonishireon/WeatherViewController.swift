02cb30111bd3449376fc603d041dfe129e671fe3
oonishireon <oonishireon110@gmail.com>
2021-02-16 11:38:27 +0900

    YumemiWeatherErrorのによって、エラーを分岐

--- oonishireon/oonishireon/oonishireon/WeatherViewController.swift
+++ oonishireon/oonishireon/oonishireon/WeatherViewController.swift
@@ -20,8 +20,18 @@ class WeatherViewController: UIViewController {
         do {
             let fetchedWeather = try YumemiWeather.fetchWeather(at: "tokyo")
             weatherImageView.image = WeatherPresentation(weatherString: fetchedWeather)?.tintedImage
+        } catch let error as YumemiWeatherError {
+            switch error {
+            case .invalidParameterError:
+                present(.alert(title: "エラー", message: "無効なパラメータが発生しました。"))
+            case .jsonDecodeError:
+                present(.alert(title: "エラー", message: "Jsonの読み込みに失敗しました。"))
+            case .unknownError:
+                present(.alert(title: "エラー", message: "不明なエラーが発生しました。"))
+            }
         } catch {
-            present(.alert(title: "エラー", message: "エラーが発生しました。"))
+            present(.alert(title: "エラー", message: "予期しないエラーが発生しました。"))
+            assertionFailure("YumemiWeatherErrorでないエラーが発生しました。")
         }
     }
 }
