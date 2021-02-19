68d8b1218533994e4f731241cf86fd7e463de815
oonishireon <oonishireon110@gmail.com>
2021-02-19 17:42:41 +0900

    不要な余白を消去

--- oonishireon/oonishireon/oonishireon/WeatherViewController.swift
+++ oonishireon/oonishireon/oonishireon/WeatherViewController.swift
@@ -14,10 +14,6 @@ class WeatherViewController: UIViewController {
     @IBOutlet weak private var minTemperatureLabel: UILabel!
     @IBOutlet weak private var maxTemperatureLabel: UILabel!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
-    
     @IBAction func tappedReloadButton(_ sender: Any) {
         do {
             let fetchedWeatherInformation = try WeatherFetcher.fetchWeatherInformation()
@@ -34,8 +30,10 @@ class WeatherViewController: UIViewController {
             
         } catch YumemiWeatherError.unknownError {
             present(.createAlert(title: "エラー", message: "不明なエラーが発生しました。"))
+            
         } catch let DecodingError.dataCorrupted(context) {
             present(.createAlert(title: "エラー", message: "JsonDecodeに失敗しました。"))
+            
         } catch {
             present(.createAlert(title: "エラー", message: "予期しないエラーが発生しました。"))
             assertionFailure("YumemiWeatherErrorでないエラーが発生しました。")
